e993301c35d00cbd3f5e67dfe3b9e76b78f987d3
videokurs12 <airincars@gmail.com>
2021-09-16 18:30:12 +0300

    Динамически изменяем модели

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <button v-on:click="addLike">likes</button> <!-- v-on:click= слушатель события -->
+  <button @click="addDislike">Dislikes</button> <!-- v-on:click= слушатель события, новый синтаксис -->
+  <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+  <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes +=1;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
