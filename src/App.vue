994b11b5204920950fe6420e996d3c2d4ac49ad9
videokurs12 <airincars@gmail.com>
2021-09-16 20:25:17 +0300

    Динамически добавляем какие-то новые посты в наш массив. Двухстороннее связывание

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
-<div>
+<div class="app">
+  <form @submit.prevent>  <!-- Что бы не перезагружалась страница -->
+    <h4>Создание поста</h4>
+    <input
+        v-bind:value="title"
+        @input="inputTitle"
+        class="input"
+        type="text"
+        placeholder="Название"
+    > <!-- v-bind: связывает какие-то данные с каким-то конкретным компонентом.
+       Отслеживает изменения в инпуте:
+       Вариант 1 : @input="inputTitle" и ниже создаем функцию
+       Вариант 2 : @input="title = $event.target.value" и все, но у меня этот вариант не сработал-->
+    <input
+        v-bind:value="body"
+        @input="inputBody"
+        class="input"
+        type="text"
+        placeholder="Описание"
+    >
+    <button
+        class="btn"
+        @click="createPost"
+    >
+      Создать
+    </button> <!-- Создаем слушатель нажатия на кнопку -->
+  </form>
   <div class="post" v-for="post in posts">
     <div><strong>Название:</strong> {{ post.title }}</div>
     <div><strong>Описание:</strong> {{ post.body }}</div>
@@ -15,11 +41,26 @@ export default {
         {id: 1, title: 'Javascript 1', body: 'Описание поста 1'},
         {id: 1, title: 'Javascript 2', body: 'Описание поста 2'},
         {id: 1, title: 'Javascript 3', body: 'Описание поста 3'},
-      ]
+      ],
+      title: '',
+      body: '',
     }
   },
   methods: {
-
+    createPost(event) {
+      const newPost = { // Добавляем новую запись
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost);
+    },
+    inputTitle(event) {
+      this.title = event.target.value; // <!-- Помещаем данные из инпута в переменную -->
+    },
+    inputBody(event) {
+      this.body = event.target.value; // <!-- Помещаем данные из инпута в переменную -->
+    }
   }
 }
 </script>
@@ -31,11 +72,37 @@ export default {
   box-sizing: border-box;
 }
 
+.app {
+  padding: 20px;
+}
+
 .post {
   padding: 15px;
   border: 2px solid teal;
   margin-top: 15px;
 }
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn {
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
+
 
 
 </style>
