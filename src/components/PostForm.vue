1e0076fef1d6b486781c008964fae756a8c421cf
videokurs12 <airincars@gmail.com>
2021-09-17 10:26:45 +0300

    Настраиваем авторегистрацию компонентов для Input

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,18 +1,16 @@
 <template>
   <form @submit.prevent>  <!-- Что бы не перезагружалась страница -->
     <h4>Создание поста</h4>
-    <input
+    <my-input
         v-model="post.title"
-        class="input"
         type="text"
         placeholder="Название"
-    > <!-- Двухстороннее связывание с использованием диррективы v-model="post.title" -->
-    <input
+    /> <!-- Двухстороннее связывание с использованием диррективы v-model="post.title" -->
+    <my-input
         v-model="post.body"
-        class="input"
         type="text"
         placeholder="Описание"
-    >
+    />
     <my-button
         style="align-self: flex-end; margin-top: 15px;"
         @click="createPost"
@@ -49,13 +47,6 @@ export default {
 form {
   display: flex;
   flex-direction: column;
-}
-
-.input {
-  width: 100%;
-  border: 1px solid teal;
-  padding: 10px 15px;
-  margin-top: 15px;
 }
 
 
