ece580830f04c75bce688a0b3a7a5854b70038de
videokurs12 <airincars@gmail.com>
2021-09-17 09:14:21 +0300

    Делаем еще один компонент отдельный пост

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="post" v-for="post in posts">
-    <div><strong>Название:</strong> {{ post.title }}</div>
-    <div><strong>Описание:</strong> {{ post.body }}</div>
+  <div>
+    <h3>Список постов</h3>
+    <post-item
+        v-for="post in posts"
+        :post="post"
+    />
   </div>
 </template>
 
 <script>
+import PostItem from "./PostItem";
 export default {
+  components: {PostItem},
   props: {
     posts: { // Пропсы. В дочернем компоненте изменять нельзя
       type: Array,
