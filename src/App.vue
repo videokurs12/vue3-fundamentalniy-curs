f085cf4c7494160db7945650488c54c3b60d4e6d
videokurs12 <airincars@gmail.com>
2021-09-18 12:30:42 +0300

    Сортировка Вариант 2

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
   </my-dialog>
 
   <post-list
-      :posts="posts"
+      :posts="sortedPosts"
       @remove="removePost"
       v-if="!isPostsLoading"
   />
@@ -77,12 +77,13 @@ export default {
   mounted() {
     this.fetchPosts()
   },
-  watch: {
-    selectedSort(newValue) {
-      this.posts.sort((post1, post2) => {
-        return post1[newValue]?.localeCompare(post2[newValue])
-      })
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+      }
     },
+  watch: {
+
   }
 }
 </script>
